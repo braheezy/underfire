84212d4b848d3ef15400125e03307dcbdf812f8e
braheezy <11700503+braheezy@users.noreply.github.com>
2025-05-15 21:46:33 -0700

    zig 0.14.0 support

--- zig/build.zig
+++ zig/build.zig
@@ -4,16 +4,18 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    // This creates another `std.Build.Step.Compile`, but this one builds an executable
-    // rather than a static library.
-    const exe = b.addExecutable(.{
-        .name = "underfire-zig",
+    const exe_mod = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
-        .root_source_file = b.path("src/main.zig"),
     });
 
-    const raylib_dep = b.dependency("raylib-zig", .{
+    const exe = b.addExecutable(.{
+        .name = "underfire-zig",
+        .root_module = exe_mod,
+    });
+
+    const raylib_dep = b.dependency("raylib_zig", .{
         .target = target,
         .optimize = optimize,
     });
@@ -23,8 +25,8 @@ pub fn build(b: *std.Build) void {
     const raylib_artifact = raylib_dep.artifact("raylib"); // raylib C library
 
     exe.linkLibrary(raylib_artifact);
-    exe.root_module.addImport("raylib", raylib);
-    exe.root_module.addImport("raygui", raygui);
+    exe_mod.addImport("raylib", raylib);
+    exe_mod.addImport("raygui", raygui);
 
     b.installArtifact(exe);
 
